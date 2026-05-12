5408dec55d5a093b49c48c4832d380e7f9f517d9
miladirimeh <74622144+miladirimeh@users.noreply.github.com>
2026-05-12 15:44:36 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,17 @@ const connectDb = require('./config/connectionDb')
 const cors = require ('cors')
 const PORT = process.env.PORT || 3000
 connectDb()
+app.use(cors({
+  origin: "https://food-recipe-5-ltf8.onrender.com/", // L'URL de ton frontend Render
+   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json())
 app.use(cors())
 app.use(express.static("public"))
 app.use("/", require("./routes/user"))
 app.use("/recipe", require('./routes/recipe'))
-app.use(cors({
-  origin: "https://food-recipe-5-ltf8.onrender.com/" // L'URL de ton frontend Render
-}));
+
 app.listen(PORT,(err)=>{
 if (err) console.log("Erreur au démarrage:", err);
 console.log(`app is listening on PORT ${PORT}`)
