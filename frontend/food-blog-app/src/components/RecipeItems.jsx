0a65d96e0ccadfe37e61636b470faec55bc601b1
miladirimeh <74622144+miladirimeh@users.noreply.github.com>
2026-05-12 15:34:34 +0200

    Update RecipeItems.jsx

--- frontend/food-blog-app/src/components/RecipeItems.jsx
+++ frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -17,7 +17,7 @@ export default function RecipeItems() {
     setAllRecipes(recipes)
     },[recipes])
   const onDelete=async(id)=>{
-  await axios.delete(`http://localhost:5000/recipe/${id}`)
+  await axios.delete(`https://food-recipe-4-qk7s.onrender.com/recipe/${id}`)
     .then ((res)=>console.log(res))
     setAllRecipes(recipes=>recipes.filter(recipe=>recipe._id !== id))
         let filterItem = favItems.filter(recipe=>recipe._id !== id)
@@ -36,7 +36,7 @@ export default function RecipeItems() {
     {
       allRecipes?.map((item,index)=>{
         return( <div key={index} className='card'>
-       <img src={`http://localhost:5000/images/${item.coverImage}`} width="120px" height="100px"></img>
+       <img src={`https://food-recipe-4-qk7s.onrender.com/images/${item.coverImage}`} width="120px" height="100px"></img>
        <div className='card-body'>
         <div className='title'>{item.title}</div>
         <div className='icons'>
